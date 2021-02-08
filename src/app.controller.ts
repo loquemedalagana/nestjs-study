3712c9139b198eda589deda2223825634dac53c2
loquemedalagana <nadamasquetu012@gmail.com>
2021-02-08 15:53:47 +0900

    라우터 추가해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/hello')
+  getHola(): string {
+    return this.appService.getHola();
+  }
 }
