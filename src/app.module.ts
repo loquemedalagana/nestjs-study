57a52481eb6532f8203e76b57576aaeef90175b9
loquemedalagana <nadamasquetu012@gmail.com>
2021-02-09 18:16:56 +0900

    add app module for homepage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { VideosController } from './videos/videos.controller';
-import { VideosService } from './videos/videos.service';
+import { VideosModule } from './videos/videos.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [VideosController],
-  providers: [VideosService],
+  imports: [VideosModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
